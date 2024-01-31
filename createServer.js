e87fda2640422a5902778b09e50eafc07d6bcee2
maurya-anshul <mauryanshul93@gmail.com>
2024-01-31 15:15:26 +0530

    request url and response added

--- createServer.js
+++ createServer.js
@@ -1,9 +1,19 @@
-const http = require('http');
+const http = require("http");
 
-// Create an HTTP server
-const server = http.createServer((request, response) => {
-  response.end('Anshul Maurya\n');
-});
-
-// Start the server and specify the port to listen on
-server.listen(4000, '127.0.0.1');
+http
+  .createServer((request, response) => {
+    switch (request.url) {
+      case "/home":
+        response.end("<h1>Welcome Home</h1>");
+        break;
+      case "/about":
+        response.end("<h1>Welcome to about</h1>");
+        break;
+      case "/node":
+        response.end("<h1>Welcome to My NodeJs Project</h1>");
+        break;
+      default:
+        response.writeHead(404);
+    }
+  })
+  .listen(4000);
