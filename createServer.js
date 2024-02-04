6cccb0adeebf26fdf2e3ac7aab08261477e4a632
maurya-anshul <mauryanshul93@gmail.com>
2024-02-04 21:02:15 +0530

    writing and reading from file added

--- createServer.js
+++ createServer.js
@@ -1,19 +1,46 @@
 const http = require("http");
-
-http
-  .createServer((request, response) => {
-    switch (request.url) {
-      case "/home":
-        response.end("<h1>Welcome Home</h1>");
-        break;
-      case "/about":
-        response.end("<h1>Welcome to about</h1>");
-        break;
-      case "/node":
-        response.end("<h1>Welcome to My NodeJs Project</h1>");
-        break;
-      default:
-        response.writeHead(404);
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body id="body"><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk.toString());
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const pasreBody = Buffer.concat(body).toString();
+      const message = pasreBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+  fs.readFile("message.txt", "utf-8", (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/html" });
+      res.end("Internal Server Error");
+      console.error("Error reading file:", err);
+      return;
     }
-  })
-  .listen(4000);
+    res.writeHead(200, { "Content-Type": "text/html" });
+    console.log(data);
+    res.write("<html>");
+    res.write(`<body><h1>${data}</h1></body>`);
+    res.write("</html>");
+    return res.end();
+  });
+});
+server.listen(4000);
