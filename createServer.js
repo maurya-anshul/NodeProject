ac9ff1e028b6c70e66961fb30674c8896fb8bec0
maurya-anshul <mauryanshul93@gmail.com>
2024-02-04 21:21:08 +0530

    writing and reading from file added

--- createServer.js
+++ createServer.js
@@ -4,13 +4,23 @@ const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
   if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write(
-      '<body id="body"><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-    );
-    res.write("</html>");
-    return res.end();
+    fs.readFile("message.txt", "utf-8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/html" });
+        res.end("Internal Server Error");
+        console.error("Error reading file:", err);
+        return;
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write("<html>");
+      res.write("<head><title>Enter Message</title></head>");
+      res.write(`<body>${data}</body>`);
+      res.write(
+        '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+      );
+      res.write("</html>");
+      return res.end();
+    });
   }
   if (url === "/message" && method === "POST") {
     const body = [];
@@ -28,19 +38,5 @@ const server = http.createServer((req, res) => {
       });
     });
   }
-  fs.readFile("message.txt", "utf-8", (err, data) => {
-    if (err) {
-      res.writeHead(500, { "Content-Type": "text/html" });
-      res.end("Internal Server Error");
-      console.error("Error reading file:", err);
-      return;
-    }
-    res.writeHead(200, { "Content-Type": "text/html" });
-    console.log(data);
-    res.write("<html>");
-    res.write(`<body><h1>${data}</h1></body>`);
-    res.write("</html>");
-    return res.end();
-  });
 });
 server.listen(4000);
